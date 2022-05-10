1da71e9be93036d0a45c1294c1c4ead446e9e389
Christina Ridlen <csridlen@utexas.edu>
2022-05-10 15:00:02 -0500

    working on model

--- code/word2vec_functions.py
+++ code/word2vec_functions.py
@@ -1,3 +1,5 @@
+from h2o.estimators.word2vec import H2OWord2vecEstimator
+
 STOP_WORDS = ["w/","at","from","in","to","/","*","-","w","+","and","&", "near", "next"]
 
 def tokenize(sentences, stop_word = STOP_WORDS):
@@ -7,4 +9,3 @@ def tokenize(sentences, stop_word = STOP_WORDS):
     tokenized_words = tokenized_filtered[tokenized_filtered.grep("[0-9]",invert=True,output_logical=True),:]
     tokenized_words = tokenized_words[(tokenized_words.isna()) | (~ tokenized_words.isin(STOP_WORDS)),:]
     return tokenized_words
-
