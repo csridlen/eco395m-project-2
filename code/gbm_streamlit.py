39ae964205f031305d0528cdf6e7a114cc7ace9f
Abby Johnson <abby.johnson@utexas.edu>
2022-05-13 13:02:21 -0500

    gbm_streamlit

--- code/gbm_streamlit.py
+++ code/gbm_streamlit.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd 
 import plotly.express as px
-from word2vec_functions import *
 import h2o
 h2o.init()
 import numpy as np
@@ -19,97 +18,97 @@ import matplotlib.pyplot as plt
 from readtable import *
 
 # Data
-airbnb = get_dists()
+# airbnb = get_dists()
 
-airbnb['host_since'] = (pd.to_datetime(airbnb['last_scraped']) - pd.to_datetime(airbnb['host_since'])) / np.timedelta64(1, 'D')
-# columns we don't need for ML model
-drop = ['id', 'month', 'last_scraped', 'host_id', 'host_name', 'neighbourhood',
-       'latitude', 'longitude',
-       'first_review', 'last_review',
-       'scrape_batch', 'batch_YRMO',
-       'instant_bookable',
-       'host_location',
-       'host_is_superhost',
-       'cum_sum',
-       'has_availability',
-       'amenities',
-       'neighbourhood_cleansed',
-       'neighbourhood_group_cleansed',
-       'avg_lat',
-       'avg_lon',
-       'last_app',
-       'List_month_byhost_month',
-    'List_month_host_overall',
-    'List_month_id_overall',
-       'List_month_byneigh',
-       'property_type']
-data = airbnb.drop(drop, axis = 1)
-data.dtypes
-dict_ints = {"List_month": float,
-            'host_is_superhost_dum': float,
-             'room_type_dum':float,
-             'instant_bookable_dum':float,
-             'hotel_dum':float,
-            'bathrooms': float}
-data = data.astype(dict_ints)
-X = data.dropna(subset = ['price']).loc[:, data.columns != 'price']
-y = data['price'].dropna()
+# airbnb['host_since'] = (pd.to_datetime(airbnb['last_scraped']) - pd.to_datetime(airbnb['host_since'])) / np.timedelta64(1, 'D')
+# # columns we don't need for ML model
+# drop = ['id', 'month', 'last_scraped', 'host_id', 'host_name', 'neighbourhood',
+#        'latitude', 'longitude',
+#        'first_review', 'last_review',
+#        'scrape_batch', 'batch_YRMO',
+#        'instant_bookable',
+#        'host_location',
+#        'host_is_superhost',
+#        'cum_sum',
+#        'has_availability',
+#        'amenities',
+#        'neighbourhood_cleansed',
+#        'neighbourhood_group_cleansed',
+#        'avg_lat',
+#        'avg_lon',
+#        'last_app',
+#        'List_month_byhost_month',
+#     'List_month_host_overall',
+#     'List_month_id_overall',
+#        'List_month_byneigh',
+#        'property_type']
+# data = airbnb.drop(drop, axis = 1)
+# data.astype(str)
+# dict_ints = {"List_month": float,
+#             'host_is_superhost_dum': float,
+#              'room_type_dum':float,
+#              'instant_bookable_dum':float,
+#              'hotel_dum':float,
+#             'bathrooms': float}
+# data = data.astype(dict_ints)
+# X = data.dropna(subset = ['price']).loc[:, data.columns != 'price']
+# y = data['price'].dropna()
 
-# One hot encoding
-# which variables are categorical?
-dict_cats = {"host_response_time": "category",
-             "room_type": "category"}
+# # One hot encoding
+# # which variables are categorical?
+# dict_cats = {"host_response_time": "category",
+#              "room_type": "category"}
 
-# drop amenities for now
-# Need to make host_response rate as int
-X['host_response_rate'] = X['host_response_rate'].str[:-1].astype(float)
+# # drop amenities for now
+# # Need to make host_response rate as int
+# X['host_response_rate'] = X['host_response_rate'].str[:-1].astype(float)
 
-X = X.astype(dict_cats)
-encoder = OneHotEncoder(handle_unknown = "ignore")
-encoder_hrt = pd.DataFrame(encoder.fit_transform(X[["host_response_time"]]).toarray())
-encoder_hrt.columns = list(X["host_response_time"].unique())
+# X = X.astype(dict_cats)
+# encoder = OneHotEncoder(handle_unknown = "ignore")
+# encoder_hrt = pd.DataFrame(encoder.fit_transform(X[["host_response_time"]]).toarray())
+# encoder_hrt.columns = list(X["host_response_time"].unique())
 
-# Merge with original dataset
-X = X.join(encoder_hrt)
+# # Merge with original dataset
+# X = X.join(encoder_hrt)
 
-# Do same for room type
-encoder_rt = pd.DataFrame(encoder.fit_transform(X[["room_type"]]).toarray())
-encoder_rt.columns = list(X["room_type"].unique())
+# # Do same for room type
+# encoder_rt = pd.DataFrame(encoder.fit_transform(X[["room_type"]]).toarray())
+# encoder_rt.columns = list(X["room_type"].unique())
 
-X = X.join(encoder_rt)
+# X = X.join(encoder_rt)
 
-## Drop original variables
-original = list(dict_cats.keys())
-X = X.drop(original, axis = 1)
+# ## Drop original variables
+# original = list(dict_cats.keys())
+# X = X.drop(original, axis = 1)
 
-X.columns = X.columns.astype(str)
-drop2 = ["nan",
-       "first_appearance",
-       "review_scores_accuracy",
-       "within a few hours",
-       "Hotel room",
-       "a few days or more",
-       "Private room",
-       "within an hour",
-       "Entire home/apt",
-       "within a day",
-       "hotel_dum",
-       "bathrooms",
-       "List_month"]
-X = X.drop(drop2, axis = 1) # drop variables that aren't important
-print(X.dtypes)
-X = X.fillna(0)
+# X.columns = X.columns.astype(str)
+# drop2 = ["nan",
+#        "first_appearance",
+#        "review_scores_accuracy",
+#        "within a few hours",
+#        "Hotel room",
+#        "a few days or more",
+#        "Private room",
+#        "within an hour",
+#        "Entire home/apt",
+#        "within a day",
+#        "hotel_dum",
+#        "bathrooms",
+#        "List_month"]
+# X = X.drop(drop2, axis = 1) # drop variables that aren't important
+# #print(X.dtypes)
+# X = X.fillna(0)
 
-# Split into train and test
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
-params = {
-    "n_estimators": 500,
-    "max_depth": 5,
-    "min_samples_split": 5,
-    "learning_rate": 0.01,
-    "loss": "squared_error"}
-reg = ensemble.GradientBoostingRegressor(**params)
-gbm_model = reg.fit(X_train, y_train)
+# # Split into train and test
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+# params = {
+#     "n_estimators": 50,
+#     "max_depth": 5,
+#     "min_samples_split": 5,
+#     "learning_rate": 0.01,
+#     "loss": "squared_error"}
+# reg = ensemble.GradientBoostingRegressor(**params)
+# gbm_model = reg.fit(X_train, y_train)
 
 
 # host_since = st.number_input('Host Since', 0, 2022, 0)
@@ -131,6 +130,7 @@ gbm_model = reg.fit(X_train, y_train)
 #neighborhood = st.selectbox(
      # 'Neighborhood',
      # ('Allerton', "Annadale-Huguenot-Prince's Bay-Woodrow", 'Arden Heights-Rossville', 'Astoria (Central)', 'Astoria (East)-Woodside (North)', 'Astoria (North)-Ditmars-Steinway', 'Astoria Park', 'Auburndale', 'Baisley Park', 'Bath Beach', 'Bay Ridge', 'Bay Terrace-Clearview', 'Bayside', 'Bedford Park', 'Bedford-Stuyvesant (East)', 'Bedford-Stuyvesant (West)', 'Bellerose', 'Belmont', 'Bensonhurst', 'Borough Park', 'Breezy Point-Belle Harbor-Rockaway Park-Broad Channel', 'Brighton Beach', 'Brooklyn Heights', 'Brooklyn Navy Yard', 'Brownsville', 'Bushwick (East)', 'Bushwick (West)', 'Calvary & Mount Zion Cemeteries', 'Calvert Vaux Park', 'Cambria Heights', 'Canarsie', 'Canarsie Park & Pier', 'Carroll Gardens-Cobble Hill-Gowanus-Red Hook', 'Castle Hill-Unionport', 'Central Park', 'Chelsea-Hudson Yards', 'Chinatown-Two Bridges', 'Claremont Park', 'Claremont Village-Claremont (East)', 'Clinton Hill', 'Co-op City', 'College Point', 'Concourse-Concourse Village', 'Coney Island-Sea Gate', 'Corona', 'Crotona Park', 'Crotona Park East', 'Crown Heights (North)', 'Crown Heights (South)', 'Cypress Hills', 'Douglaston-Little Neck', 'Downtown Brooklyn-DUMBO-Boerum Hill', 'Dyker Heights', 'East Elmhurst', 'East Flatbush-Erasmus', 'East Flatbush-Farragut', 'East Flatbush-Remsen Village', 'East Flatbush-Rugby', 'East Flushing', 'East Harlem (North)', 'East Harlem (South)', 'East Midtown-Turtle Bay', 'East New York (North)', 'East New York-City Line', 'East New York-New Lots', 'East Village', 'East Williamsburg', 'Eastchester-Edenwald-Baychester', 'Elmhurst', 'Far Rockaway-Bayswater', 'Financial District-Battery Park City', 'Flatbush', 'Flatbush (West)-Ditmas Park-Parkville', 'Flatlands', 'Flushing Meadows-Corona Park', 'Flushing-Willets Point', 'Fordham Heights', 'Forest Hills', 'Fort Greene', 'Fort Hamilton', 'Fresh Meadows-Utopia', 'Glen Oaks-Floral Park-New Hyde Park', 'Glendale', 'Gramercy', 'Grasmere-Arrochar-South Beach-Dongan Hills', 'Gravesend (East)-Homecrest', 'Gravesend (South)', 'Gravesend (West)', 'Great Kills-Eltingville', 'Greenpoint', 'Greenwich Village', 'Hamilton Heights-Sugar Hill', 'Harlem (North)', 'Harlem (South)', "Hell's Kitchen", 'Highbridge', 'Highbridge Park', 'Highland Park-Cypress Hills Cemeteries (North)', 'Hollis', 'Holy Cross Cemetery', 'Howard Beach-Lindenwood', 'Hunts Point', 'Hutchinson Metro Center', 'Inwood', 'Inwood Hill Park', 'Jackson Heights', 'Jacob Riis Park-Fort Tilden-Breezy Point Tip', 'Jamaica', 'Jamaica Estates-Holliswood', 'Jamaica Hills-Briarwood', 'Kensington', 'Kew Gardens', 'Kew Gardens Hills', 'Kingsbridge Heights-Van Cortlandt Village', 'Kingsbridge-Marble Hill', 'Kissena Park', 'Laurelton', 'Lincoln Terrace Park', 'Long Island City-Hunters Point', 'Longwood', 'Lower East Side', 'Madison', 'Manhattanville-West Harlem', 'Mapleton-Midwood (West)', 'Marine Park-Mill Basin-Bergen Beach', "Mariner's Harbor-Arlington-Graniteville", 'Maspeth', 'Melrose', 'Middle Village', 'Middle Village Cemetery', 'Midtown South-Flatiron-Union Square', 'Midtown-Times Square', 'Midwood', 'Montefiore Cemetery', 'Morningside Heights', 'Morris Park', 'Morrisania', 'Mott Haven-Port Morris', 'Mount Eden-Claremont (West)', 'Mount Hebron & Cedar Grove Cemeteries', 'Mount Hope', 'Mount Olivet & All Faiths Cemeteries', 'Murray Hill-Broadway Flushing', 'Murray Hill-Kips Bay', 'New Dorp-Midland Beach', 'New Springville-Willowbrook-Bulls Head-Travis', 'North Corona', 'Norwood', 'Oakland Gardens-Hollis Hills', 'Oakwood-Richmondtown', 'Ocean Hill', 'Old Astoria-Hallets Point', 'Ozone Park', 'Ozone Park (North)', 'Park Slope', 'Parkchester', 'Pelham Bay Park', 'Pelham Bay-Country Club-City Island', 'Pelham Gardens', 'Pelham Parkway-Van Nest', 'Pomonok-Electchester-Hillcrest', 'Port Richmond', 'Prospect Heights', 'Prospect Lefferts Gardens-Wingate', 'Prospect Park', 'Queens Village', 'Queensboro Hill', 'Queensbridge-Ravenswood-Dutch Kills', 'Rego Park', 'Richmond Hill', 'Ridgewood', 'Riverdale-Spuyten Duyvil', 'Rockaway Beach-Arverne-Edgemere', 'Rockaway Community Park', 'Rosebank-Shore Acres-Park Hill', 'Rosedale', 'Sheepshead Bay-Manhattan Beach-Gerritsen Beach', 'SoHo-Little Italy-Hudson Square', 'Soundview Park', 'Soundview-Bruckner-Bronx River', 'Soundview-Clason Point', 'South Jamaica', 'South Ozone Park', 'South Richmond Hill', 'South Williamsburg', 'Spring Creek-Starrett City', 'Springfield Gardens (North)-Rochdale Village', 'Springfield Gardens (South)-Brookville', 'St. Albans', 'St. George-New Brighton', 'St. John Cemetery', 'Stuyvesant Town-Peter Cooper Village', 'Sunnyside', 'Sunnyside Yards (North)', 'Sunnyside Yards (South)', 'Sunset Park (Central)', 'Sunset Park (East)-Borough Park (West)', 'Sunset Park (West)', 'The Battery-Governors Island-Ellis Island-Liberty Island', 'The Evergreens Cemetery', 'Throgs Neck-Schuylerville', 'Todt Hill-Emerson Hill-Lighthouse Hill-Manor Heights', 'Tompkinsville-Stapleton-Clifton-Fox Hills', 'Tottenville-Charleston', 'Tremont', 'Tribeca-Civic Center', 'United Nations', 'University Heights (North)-Fordham', 'University Heights (South)-Morris Heights', 'Upper East Side-Carnegie Hill', 'Upper East Side-Lenox Hill-Roosevelt Island', 'Upper East Side-Yorkville', 'Upper West Side (Central)', 'Upper West Side-Lincoln Square', 'Upper West Side-Manhattan Valley', 'Van Cortlandt Park', 'Wakefield-Woodlawn', 'Washington Heights (North)', 'Washington Heights (South)', 'West Farms', 'West New Brighton-Silver Lake-Grymes Hill', 'West Village', 'Westchester Square', 'Westerleigh-Castleton Corners', 'Whitestone-Beechhurst', 'Williamsbridge-Olinville', 'Williamsburg', 'Windsor Terrace-South Slope', 'Woodhaven', 'Woodside', 'Yankee Stadium-Macombs Dam Park'))
+        
         
 @st.cache()
   
